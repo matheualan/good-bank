bf7774aba1a1b6fe9d5f47dfc2983e8bfbca15e3
maths <matheusalan.dev@gmail.com>
2026-01-24 03:08:49 -0300

    Iniciando configurações iniciais do projeto. Criado estrutura de pastas para as entidades Person e Account

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  process.env.TZ = '-03:00';
+  // app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors();
   await app.listen(process.env.PORT ?? 4200);
 }
 bootstrap();
