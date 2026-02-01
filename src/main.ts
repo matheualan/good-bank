41490b2459c88e4feec6ef95f83eb4e62fc63f51
maths <matheusalan.dev@gmail.com>
2026-02-01 01:14:41 -0300

    Implementação documentação swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+  .setTitle('Good Bank - Seu Banco de Confiança')
+  .setDescription('Gerenciamento de transações bancárias')
+  .setContact("Matheus Alan","http://linkedin.com/in/matheualan","matheusalan.dev@gmail.com")
+  .setVersion('1.0')
+  // .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   process.env.TZ = '-03:00';
-
   app.useGlobalPipes(new ValidationPipe());
-
   app.enableCors();
   
-  await app.listen(process.env.PORT ?? 4200);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
